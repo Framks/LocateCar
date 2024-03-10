eb7b043f749713314e3e4020f46504d3deea8d16
Francisco Gabriel <galvesa9@gmail.com>
2024-03-09 21:09:17 -0300

    criando o banco de dados

--- src/infra/db/BancoDeDados.java
+++ src/infra/db/BancoDeDados.java
@@ -1,5 +1,39 @@
 package infra.db;
 
+import java.util.*;
+
 public class BancoDeDados {
+
+    private static final HashMap OBJETOS = new HashMap();
+    private Long id;
+
+    public void inserirObjeto(Object objeto) throws Exception {
+        Set objetos = colecaoDeObjetos(objeto.getClass());
+        objetos.add(objeto);
+    }
+
+    public void excluirObjeto(Object objeto) {
+        Set objetos = colecaoDeObjetos(objeto.getClass());
+        objetos.remove(objeto);
+    }
+
+    public List buscarObjetosPorTipo(Class clas) {
+        Set objetos = colecaoDeObjetos(clas);
+        return new ArrayList(objetos);
+    }
+
+    private Set colecaoDeObjetos(Class clas) {
+        Set objetos = (Set) BancoDeDados.OBJETOS.get(clas);
+        if (objetos == null) {
+            objetos = new HashSet();
+            BancoDeDados.OBJETOS.put(clas, objetos);
+        }
+        return objetos;
+    }
+
+    public Long proximoId() {
+        id = id + 1;
+        return id;
+    }
 
 }
