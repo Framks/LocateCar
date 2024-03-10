bfc2f4776243687c470c8fe51c50a8637e220869
Francisco Gabriel <galvesa9@gmail.com>
2024-03-09 21:10:14 -0300

    criando repositorio abstrato

--- src/infra/repository/RepositorioAbstrato.java
+++ src/infra/repository/RepositorioAbstrato.java
@@ -1,4 +1,15 @@
 package infra.repository;
 
-public class RepositorioAbstrato {
+import java.util.List;
+
+public interface RepositorioAbstrato<T> {
+    public void salva(T objeto);
+
+    public void deletar(T objeto);
+
+    public List<T> buscar();
+
+    public T buscarporId(Long id);
+
+    public void atualizar(T objeto);
 }
