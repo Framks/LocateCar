3d613b5f6ae5ae789772c641c484b37c9d58a761
Francisco Gabriel <galvesa9@gmail.com>
2024-03-12 20:58:53 -0300

    trabalhando a view de aluguel

--- src/view/Aluguel/ViewAluguel.java
+++ src/view/Aluguel/ViewAluguel.java
@@ -15,6 +15,35 @@ public class ViewAluguel {
         this.entrada = entrada;
     }
     public void run(){
+        Integer option;
+        boolean execucao = true;
+        while (execucao){
+            System.out.print("##    Aluguéis     ##\n" +
+                    "    0 - Sair: \n" +
+                    "    1 - Alugar Veiculo: \n" +
+                    "    2 - Devolver Veiculo: \n" +
+                    "    3 - Listar Todos Alugueis: \n"+
+                    "Digite a opção para entrar: ");
+            option = this.entrada.receberInteger();
+            switch (option){
+                case 0-> execucao = false;
+                case 1 -> alugar();
+                case 2 -> devolver();
+                case 3 -> listar();
+                default -> System.out.println("entrada não correspondente a opções");
+            }
+        }
+    }
+
+    private void alugar(){
+
+    }
+
+    private void devolver(){
+
+    }
+
+    private void listar(){
 
     }
 }
