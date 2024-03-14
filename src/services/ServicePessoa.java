51934966184075ec8f219698eebc86313783d19d
Francisco Gabriel <galvesa9@gmail.com>
2024-03-14 14:05:19 -0300

    implementando funções no service pessoa

--- src/services/ServicePessoa.java
+++ src/services/ServicePessoa.java
@@ -13,27 +13,57 @@ public class ServicePessoa<T extends Pessoa> {
         this.repositorioPessoa = repositorioPessoa;
     }
 
-    public void cadastrar(T veiculo){
-
+    public void cadastrar(T pessoa){
+        if (pessoa == null)
+            throw new RuntimeException("Pessoa nulo");
+        if (pessoa.getIdentificao() == null)
+            throw new RuntimeException("identificação nula");
+        if (pessoa.getNome() == null)
+            throw new RuntimeException("nome nulo");
+        this.repositorioPessoa.gravar(pessoa);
     }
-    public void alterar(T veiculo){
-
+    public void alterar(T pessoa) throws RuntimeException{
+        if (pessoa == null)
+            throw new RuntimeException("Pessoa nulo");
+        if (pessoa.getIdentificao() == null)
+            throw new RuntimeException("Pessoa em que ter uma identificação");
+        T pessoaanterior = this.repositorioPessoa.buscarPorId(pessoa.getIdentificao());
+        this.excluir(pessoaanterior);
+        if (pessoa.getNome() != null)
+            pessoaanterior.setNome(pessoa.getNome());
+        if (pessoa.getTelefone() != null)
+            pessoaanterior.setTelefone(pessoa.getTelefone());
+        if (pessoa.getEndereco() != null)
+            pessoaanterior.setEndereco(pessoa.getEndereco());
+        this.repositorioPessoa.gravar(pessoaanterior);
     }
 
     public List<T> listar(){
         return this.repositorioPessoa.listar();
     }
 
-    public void excluir(T veiculo){
-
+    public void excluir(T pessoa){
+        if (pessoa == null || pessoa.getIdentificao() == null){
+            throw new RuntimeException("Pessoa não pode ser nulo");
+        }
+        this.repositorioPessoa.excluir(pessoa);
     }
 
     public T buscarPorNome(String nome){
+        if (nome == null)
+            throw new RuntimeException("Nome esta nulo");
+        List<T> lista = this.repositorioPessoa.listar();
+        for (T pessoa : lista){
+            if (pessoa.getNome().equals(nome))
+                return pessoa;
+        }
         return null;
     }
 
-    public T buscarPorId(Long id){
-        return null;
+    public T buscarPorId(Long identificao){
+        if (identificao == null)
+            throw new RuntimeException("Identificação nulo");
+        return this.repositorioPessoa.buscarPorId(identificao);
     }
 
 
