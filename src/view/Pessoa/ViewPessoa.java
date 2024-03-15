824e943a8d2b59d170432726ec70ac78058a1d15
Francisco Gabriel <galvesa9@gmail.com>
2024-03-15 08:37:14 -0300

    removendo classes e melhorando codigo

--- src/view/Pessoa/ViewPessoa.java
+++ src/view/Pessoa/ViewPessoa.java
@@ -15,8 +15,6 @@ public class ViewPessoa {
     private final ServicePessoa<PessoaJuridica> servPJ;
     private final ServicePessoa<PessoaFisica> servPF;
     private final EntradaVerificada entrada;
-    private final ViewPessoaFisica viewPF;
-    private final ViewPessoaJuridica viewPJ;
 
     public ViewPessoa(ServicePessoa<PessoaFisica> servPessoaPf,
                       ServicePessoa<PessoaJuridica> servicePessoaPj,
@@ -24,8 +22,6 @@ public class ViewPessoa {
         this.servPJ = servicePessoaPj;
         this.servPF = servPessoaPf;
         this.entrada = entrada;
-        this.viewPF = new ViewPessoaFisica(servPessoaPf,entrada);
-        this.viewPJ = new ViewPessoaJuridica( servicePessoaPj,entrada);
     }
 
     public void run(){
@@ -102,41 +98,122 @@ public class ViewPessoa {
             System.out.print("para cadastrar o Cliente diga se ele é Fisico(1) ou Juridico(2): ");
             Integer opcao_pessoa = this.entrada.receberInteger();
             if (opcao_pessoa == 1){
-                this.viewPF.cadastrar();
-            }else if (opcao_pessoa == 2){
-                this.viewPJ.cadastrar();
+                PessoaFisica pessoa = new PessoaFisica();
+
+                System.out.print("Digite o Cpf da pessoa: ");
+                pessoa.setIdentificao(this.entrada.receberLong());
+
+                System.out.println("Digite o nome da pessoa: ");
+                pessoa.setNome(this.entrada.receberString());
+
+                System.out.println("Digite o Telefone: ");
+                pessoa.setTelefone(this.entrada.receberString());
+
+                System.out.println("Endereço : ");
+                pessoa.setEndereco(this.entrada.receberString());
+                if(getPessoa(pessoa.getIdentificao()) != null)
+                    throw new RuntimeException("Cliente ja cadastrado: ");
+                this.servPF.cadastrar(pessoa);
+            }
+            else if (opcao_pessoa == 2){
+                PessoaJuridica pessoa = new PessoaJuridica();
+
+                System.out.print("Digite o Cnpj da empresa: ");
+                pessoa.setIdentificao(this.entrada.receberLong());
+
+                System.out.println("Digite a Razão social: ");
+                pessoa.setNome(this.entrada.receberString());
+
+                System.out.println("Digite o Telefone: ");
+                pessoa.setTelefone(this.entrada.receberString());
+
+                System.out.println("Endereço : ");
+                pessoa.setEndereco(this.entrada.receberString());
+                if (getPessoa(pessoa.getIdentificao()) != null)
+                    throw new RuntimeException("cliente ja cadastrado");
+                this.servPJ.cadastrar(pessoa);
             }
        }catch (Exception e){
             System.out.println("NÃO FOI POSSIVEL CADASTRAR O CLIENTE");
         }
     }
 
+    // para melhorar e remover bugs
     private void alterar(){
         try{
             listar();
-            System.out.println("digite 1 para alterar uma pessoa fisica e 2 para pessoa Juridica: ");
+            System.out.println("digite o cpf ou o cnpj da pessoa que deseja alterar: ");
             Integer opt = entrada.receberInteger();
             if (opt == 1){
                 System.out.println("digite o cpf da pessoa que deseja alterar: ");
                 Long cpf = this.entrada.receberLong();
-                this.viewPF.alterar(cpf);
+                PessoaFisica pessoa = this.service.buscarPorId(cpf);
+                PessoaFisica newPessoa = new PessoaFisica();
+                System.out.println("Nome: "+pessoa.getNome());
+                System.out.print("Vc deseja Mudar o nome, se sim(1) ou não(2): ");
+                Integer opcao = this.entrada.receberInteger();
+                if (opcao == 1){
+                    System.out.print("Digite o novo nome: ");
+                    newPessoa.setNome(this.entrada.receberString());
+                }
+                System.out.print("Vc deseja Mudar o telefone, se sim(1) ou não(2): ");
+                opcao = this.entrada.receberInteger();
+                if (opcao == 1){
+                    System.out.print("Digite o novo telefone: ");
+                    newPessoa.setTelefone(this.entrada.receberString());
+                }
+                System.out.print("Vc deseja Mudar o endereço, se sim(1) ou não(2): ");
+                opcao = this.entrada.receberInteger();
+                if (opcao == 1){
+                    System.out.print("Digite o novo endereço: ");
+                    newPessoa.setEndereco(this.entrada.receberString());
+                }
+                newPessoa.setIdentificao(pessoa.getIdentificao());
+                this.service.alterar(newPessoa);
             } else if (opt == 2) {
                 System.out.println("digite o cnpj da pessoa que deseja alterar: ");
                 Long cnpj = this.entrada.receberLong();
-                this.viewPJ.alterar(cnpj);
+                PessoaJuridica pessoa = this.service.buscarPorId(cnpj);
+                PessoaJuridica newPessoa = new PessoaJuridica();
+                System.out.println("Razão social: "+pessoa.getNome());
+
+                System.out.print("Vc deseja Mudar a razão social, se sim(1) ou não(2): ");
+                Integer opcao = this.entrada.receberInteger();
+                if (opcao == 1){
+                    System.out.print("Digite a razão social: ");
+                    newPessoa.setNome(this.entrada.receberString());
+                }
+
+                System.out.print("Vc deseja Mudar o telefone, se sim(1) ou não(2): ");
+                opcao = this.entrada.receberInteger();
+                if (opcao == 1){
+                    System.out.print("Digite o novo telefone: ");
+                    newPessoa.setTelefone(this.entrada.receberString());
+                }
+
+                System.out.print("Vc deseja Mudar o endereço, se sim(1) ou não(2): ");
+                opcao = this.entrada.receberInteger();
+                if (opcao == 1){
+                    System.out.print("Digite o novo endereço: ");
+                    newPessoa.setEndereco(this.entrada.receberString());
+                }
+
+                newPessoa.setIdentificao(pessoa.getIdentificao());
+                this.service.alterar(newPessoa);
             }
         }catch (Exception e){
             System.out.println("Ocorreu um erro não foi possível alterar o cliente");
         }
     }
 
-    public Pessoa getPessoa(String identificao){
+    public Pessoa getPessoa(Long identificao){
         List<PessoaJuridica> pessoaJuridicas = this.servPJ.listar();
         List<PessoaFisica> pessoaFisicas = this.servPF.listar();
 
         for (PessoaJuridica p : pessoaJuridicas)
             if (p.getIdentificao().equals(identificao))
                 return p;
+
         for (PessoaFisica p: pessoaFisicas)
             if (p.getIdentificao().equals(identificao))
                 return p;
